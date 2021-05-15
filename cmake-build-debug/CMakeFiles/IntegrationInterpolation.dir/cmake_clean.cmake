7dafec862d8d75075d9038510da649e707405a87
Zara <zara.forgest@gmail.com>
2021-05-15 18:30:50 +0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/IntegrationInterpolation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IntegrationInterpolation.dir/main.cpp.obj"
+  "IntegrationInterpolation.exe"
+  "IntegrationInterpolation.exe.manifest"
+  "IntegrationInterpolation.pdb"
+  "libIntegrationInterpolation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IntegrationInterpolation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
